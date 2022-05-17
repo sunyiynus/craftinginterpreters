77a251db7b2b5e0140cf981ffa3ec1536db62f07
Sun Yi <iuamyy@163.com>
2022-05-17 10:06:11 +0800

    Test lexer

--- test/lex_test.cpp
+++ test/lex_test.cpp
@@ -1,14 +1,16 @@
 #include "catch2/catch_test_macros.hpp"
 #include "lex.h"
+#include "types.h"
 #include <catch2/catch_all.hpp>
 #include <fstream>
+#include <iterator>
 
 TEST_CASE("Normal testing") {
-  bstring line = "";
   std::ifstream ins{"./lox_program/lox_1.lox"};
-  bello::Scanner scanner;
-  
-  while (std::getline(ins, line)) {
-      scanner.scanTokens(line);
-  }
+  bstring source{std::istreambuf_iterator<char>(ins),
+                 std::istreambuf_iterator<char>()};
+  REQUIRE_FALSE(source.empty());
+
+  bello::Scanner scanner{source};
+  auto res = scanner.scanTokens();
 }
