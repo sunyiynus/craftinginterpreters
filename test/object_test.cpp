0d2ba0d77366d6063709b883b7ea0546efbc811b
SunYi <iuamyy@163.com>
2022-12-30 13:17:05 +0800

    Testing

--- test/object_test.cpp
+++ test/object_test.cpp
@@ -8,3 +8,11 @@ TEST_CASE("Testing function MakeObject()") {
   REQUIRE(p->value == 42);
   REQUIRE(p->type == "Number");
 }
+
+
+TEST_CASE("Testing function MakeObject()") {
+      auto p = bello::MakeObject<Number>(42);
+  REQUIRE(p->value == 42);
+  REQUIRE(p->type == "Number");
+}
+
